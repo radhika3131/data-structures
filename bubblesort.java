aec7fe947a9a1e370334ba0f557ba5ffdf1f209b
Radhika Ramsen <102825662+radhika3131@users.noreply.github.com>
2023-02-11 00:49:27 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,33 @@
+package com.datastructures.Sorting;
+
+public class bubblesort {
+    public static void sort(int[] arr)
+    {
+        int n = arr.length;
+        for(int i =0 ; i<n-1; i++)
+        {
+            for(int j =0 ; j<n-i-1 ; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = { 5 , 0,1,10,6, 2};
+        sort(arr);
+
+        int n = arr.length;
+        for(int i =0 ; i<n ; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+    }
+}
